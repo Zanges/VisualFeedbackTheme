8c685c83bc4a939ad2ca242317c2302ed0c374ba
Zanges <3868054+Zanges@users.noreply.github.com>
2024-02-07 20:12:15 +0100

    cleanup an comments

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,5 +1,3 @@
 import VisualFeedback from './scripts/visual-feedback';
 
-// window.PluginManager.register('AddToCard', VisualFeedback, '.buy-widget');
-
 window.PluginManager.override('AddToCart', VisualFeedback, '[data-add-to-cart]');
--- src/Resources/app/storefront/src/scripts/visual-feedback.js
+++ src/Resources/app/storefront/src/scripts/visual-feedback.js
@@ -23,42 +23,45 @@ export default class VisualFeedback extends AddToCard {
     this._visualFeedback();
   }
 
-  _refreshCartValue() {
+  _refreshCartValue() { // needed to keep the cart value in sync with the actual cart
     const cartWidgetEl = DomAccess.querySelector(this._cartEl, '[data-cart-widget]');
     const cartWidgetInstance = this.PluginManager.getPluginInstanceFromElement(cartWidgetEl, 'CartWidget');
     cartWidgetInstance.fetch();
   }
 
   _visualFeedback() {
+    // Get the labels from the snippets via data attributes
     const feedbackLabel = this._buyButtonEl.getAttribute('data-feedback-label');
     const defaultLabel = this._buyButtonEl.getAttribute('data-add-to-cart-label');
 
-    console.log(feedbackLabel, defaultLabel);
-
+    // Disable the button and add a visual feedback class
     this._buyButtonEl.disabled = true;
     this._buyButtonEl.classList.add('btn-buy-visual-feedback');
 
-    if (feedbackLabel) {
+    if (feedbackLabel) { // check if the feedback label is set to avoid setting an empty string
       this._buyButtonEl.textContent = feedbackLabel;
     }
 
+    // Add a visual feedback class to the quantity input if you are on a product detail page
     let quantityClassAdded = false;
     if (this._quantityInputEl) {
       this._quantityInputEl.classList.add('quantity-selector-group-visual-feedback');
       quantityClassAdded = true;
     }
 
-    window.setTimeout(() => {
+    window.setTimeout(() => { // Reset the button and quantity input after 1 seconds
       this._buyButtonEl.disabled = false;
       this._buyButtonEl.classList.remove('btn-buy-visual-feedback');
       
-      if (defaultLabel) {
+      if (defaultLabel) { // check if the default label is set to avoid setting an empty string else set a default fallback label - this should never happen
         this._buyButtonEl.textContent = defaultLabel;
+      } else {
+        this._buyButtonEl.textContent = 'Add to cart';
       }
 
       if (quantityClassAdded) {
         this._quantityInputEl.classList.remove('quantity-selector-group-visual-feedback');
       }
-    }, 3000);
+    }, 1000);
   }
 }
